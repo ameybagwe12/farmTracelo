e39a67bf0781f9cf4c3c9930d043e18974ff0a12
Vailantan <vailantan@3003.com>
2024-02-25 03:59:28 +0530

    add-item

--- src/pages/Buy.jsx
+++ src/pages/Buy.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Navbar from '../Components/Nav'
+import Card1 from '../Components/Card1'
 export default function Buy() {
   return (<>
     <Navbar/>
+    <Card1/>
     </>
     
   )
