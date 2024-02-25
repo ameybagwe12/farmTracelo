d7ccfcc7bc6caea9f3224b43f1fe6bf06848fc8c
Amey Bagwe <ameybagwe09@gmail.com>
2024-02-25 05:59:33 +0530

    Track.jsx updates

--- src/pages/Track.jsx
+++ src/pages/Track.jsx
@@ -5,6 +5,31 @@ import TextField from "@mui/material/TextField";
 import "../styles/Track.css";
 
 export default function Track({ account, connectWallet }) {
+  //   async function main() {
+  //     const contractAddress = "0xcEa45Ac3CE75843ed5436b70713B20E314266a64";
+  //     const contract = await ethers.getContractAt("PT", contractAddress);
+  //     let products = [];
+  //     await recur(contract, "456", products);
+  //   }
+
+  //   async function recur(contract, id, products) {
+  //     const product = await contract.getProduct(id);
+  //     products.push(product);
+  //     console.log(product);
+
+  //     if (product.prev !== "") {
+  //       await recur(contract, product.prev);
+  //     }
+  //     console.log(products);
+  //   }
+
+  //   main()
+  //     .then(() => process.exit(0))
+  //     .catch((error) => {
+  //       console.error(error);
+  //       process.exit(1);
+  //     });
+
   return (
     <>
       <Navbar account={account} connectWallet={connectWallet} />
